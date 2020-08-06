f280872cd6fdfd96820711a9e56bdbb17b3ac6b0
stevenj2019 <steven.j2019@pm.me>
2020-08-06 16:49:52 +0100

    added image tag

--- Jenkins_scripts/build_infra.sh
+++ Jenkins_scripts/build_infra.sh
@@ -3,5 +3,5 @@ cd terraform
 terraform init 
 terraform apply -auto-approve
 cd ../kubernetes
-kubectl set image deployment/petclinic petclinic=pstyp94/backend
-kubectl set image deployment/frontend frontend=pstyp94/frontend
+kubectl set image deployment/petclinic petclinic=pstyp94/backend:latest
+kubectl set image deployment/frontend frontend=pstyp94/frontend:latest
